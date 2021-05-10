e4efa9bd71af1dc4e09f6ba1b9be27622e129f81
Elizabeth O. Segun-Ajiboye <elizabethaderemi@gmail.com>
2021-05-10 19:14:09 +0100

    managed cache location and some more

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
+// require("dotenv").config();
+// dev-t80p-j3l.us.auth0.com
+// DQ5YZkg7NJ7axWE3avbVSoTonOnPLvbIDQ5YZkg7NJ7axWE3avbVSoTonOnPLvbI
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-t80p-j3l.us.auth0.com"
+      clientId="DQ5YZkg7NJ7axWE3avbVSoTonOnPLvbI"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
